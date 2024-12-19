07ddb3dbe542ca91784b83a6f820b7129aada637
Louisan Tchitoula <t.louisan@outlook.fr>
2024-12-19 11:36:01 +0100

    FIX encrypt exe generation script

--- fatalis.sh
+++ fatalis.sh
@@ -19,6 +19,17 @@ else
   echo "PyInstaller est installé."
 fi
 
+# Vérifier si pycryptodome est installé
+if ! python -c "import Crypto" &>/dev/null; then
+  echo "pycryptodome n'est pas installé. Installation en cours..."
+  pip install pycryptodome || {
+    echo "Échec de l'installation de pycryptodome. Arrêt du script."
+    exit 1
+  }
+else
+  echo "pycryptodome est installé."
+fi
+
 # Vérifier si le répertoire du projet existe
 if [ ! -d "$PROJECT_DIR" ]; then
   echo "Le répertoire du projet '$PROJECT_DIR' est introuvable. Arrêt du script."
@@ -51,7 +62,19 @@ fi
 
 # Exécuter PyInstaller pour créer l'exécutable
 echo "Création de l'exécutable..."
-pyinstaller --onefile --add-data "$KEY_FILE:." --add-data "$ADDITIONAL_SCRIPT:." "$MAIN_SCRIPT" --name "$OUTPUT_NAME" --distpath "../dist" || {
+pyinstaller --onefile \
+  --add-data "$KEY_FILE:." \
+  --add-data "$ADDITIONAL_SCRIPT:." \
+  "$MAIN_SCRIPT" \
+  --name "$OUTPUT_NAME" \
+  --distpath "../dist" \
+  --hidden-import "Crypto" \
+  --hidden-import "Crypto.Cipher" \
+  --hidden-import "Crypto.PublicKey" \
+  --hidden-import "Crypto.Random" \
+  --hidden-import "Crypto.Util" \
+  --hidden-import "Crypto.Util.Padding" \
+  --collect-all "Crypto" || {
   echo "Échec de la création de l'exécutable. Arrêt du script."
   exit 1
 }
